8ec32712774c11e69611b607ed5762e25782228a
prasadbonela52892 <prasadbonela52892@gmail.com>
2024-05-17 09:50:46 +0000

    Upload New File

---
+++ src/main/java/com/epam/rd/autotasks/Snail.java
@@ -0,0 +1,31 @@
+package com.epam.rd.autotasks.meetastranger;
+import java.util.Scanner;
+
+public class Snail {
+    public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		int uf=sc.nextInt();
+		int df=sc.nextInt();
+		int hf=sc.nextInt();
+		int count=0;
+		int initial=0;
+		if(uf==df && uf<hf) {
+			System.out.println("Impossible");
+		}
+		else if(uf>hf) {
+			System.out.println(1);
+		}
+		else {
+		for(int i=initial;i<=hf;i++) {
+			initial=initial+(uf-df);
+			if(initial>=hf) {
+				break;
+			}
+			else {
+				count++;
+			}
+		}
+		System.out.println(count);
+	}
+	}
+}
