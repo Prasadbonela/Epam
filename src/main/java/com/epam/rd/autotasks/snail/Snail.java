6c6bd852a6a379bb3debc8fe15999a2bef74bfc4
prasadbonela52892 <prasadbonela52892@gmail.com>
2024-05-17 11:18:02 +0000

    Replace Snail.java

--- src/main/java/com/epam/rd/autotasks/snail/Snail.java
+++ src/main/java/com/epam/rd/autotasks/snail/Snail.java
@@ -1,31 +1,23 @@
 package com.epam.rd.autotasks.snail;
-import java.util.Scanner;
+import java.util.*;
 
 public class Snail {
-    public static void main(String[] args) {
-		Scanner sc=new Scanner(System.in);
-		int uf=sc.nextInt();
-		int df=sc.nextInt();
-		int hf=sc.nextInt();
-		int count=0;
-		int initial=0;
-		if(uf==df && uf<hf) {
-			System.out.println("Impossible");
-		}
-		else if(uf>hf) {
-			System.out.println(1);
-		}
-		else {
-		for(int i=initial;i<=hf;i++) {
-			initial=initial+(uf-df);
-			if(initial>=hf) {
-				break;
-			}
-			else {
-				count++;
-			}
-		}
-		System.out.println(count);
-	}
-	}
+	public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int h = sc.nextInt();
+        int days = 0;
+        int workdone = 0;
+        if (a <= b) {
+            System.out.println("Impossible");
+        } 
+        else {
+            days = (h - b) / (a - b);
+            if ((h - b) % (a - b) != 0) {
+                days += 1;
+            }
+        System.out.println(days);
+    }
+}
 }
